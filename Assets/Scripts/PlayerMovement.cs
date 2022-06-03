7d3f17e631741fd1961199643b92a5486376f748
Greg <2000247C@student.tp.edu.sg>
2022-06-04 04:44:20 +0800

    added player to main scene

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,16 +3,21 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float Speed;
+    public float Speed = 1.2f;
+    public float RotateSpeed = 180f;
+    public float Gravity = 5f;
 
     Joystick js;
 
     Transform cam;
+    float down;
 
     void Start()
     {
         js = FindObjectOfType<Joystick>();
         cam = Camera.main.transform;
+
+        down = 1f;
     }
 
     void Update()
@@ -29,6 +34,33 @@ public class PlayerMovement : MonoBehaviour
             move += transform.position;
 
             transform.position = Vector3.MoveTowards(transform.position, move, Speed * Time.deltaTime);
+
+            Quaternion lookRot = Quaternion.LookRotation((move - transform.position), Vector3.up);
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, lookRot, RotateSpeed * Time.deltaTime);
         }
+
+        // down += 0.1f;
+        // if (down > 1) down = 1;
+
+        // Vector3 downVector = transform.position - (down * Vector3.up);
+
+        // float gravity = down < 0 ? 15f : Gravity;
+
+        // if (downVector.y > 0)
+        // {
+        //     transform.position = Vector3.MoveTowards(transform.position, downVector, gravity * Time.deltaTime);
+        // }
+        // else
+        // {
+        //     Vector3 minimumDown = new Vector3(transform.position.x, 0, transform.position.z);
+
+        //     transform.position = Vector3.MoveTowards(transform.position, minimumDown, gravity * Time.deltaTime);
+        // }
+    }
+
+    public void Jump()
+    {
+        down = -5f;
     }
 }
